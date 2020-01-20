1d2d57ae537da94a8d67da4e207edb8799ce7c73
Brennon Nuckols <brennon@Brennons-MacBook-Pro.local>
2020-01-19 20:48:48 -0700

    card component built

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,13 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get("https://api.github.com/users/brennuck")
+.then(response => {
+  console.log(response);
+})
+.catch(error => {
+  console.log(error);
+})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -53,3 +60,50 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+function userCard(object) {
+  // create new elements
+  const cardDiv = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const cardName = document.createElement('h3');
+  const userName = document.createElement('p');
+  const userLocation = document.createElement('p');
+  const userProfile = document.createElement('p');
+  const profileAddress = document.createElement('a');
+  const userFollowers = document.createElement('p');
+  const userFollowing = document.createElement('p');
+  const userBio = document.createElement('p');
+
+  // setup the structure of our element
+  cardDiv.append(cardImg);
+  cardDiv.append(cardInfo);
+  cardInfo.append(cardName);
+  cardInfo.append(userName);
+  cardInfo.append(userLocation);
+  cardInfo.append(userProfile);
+  cardInfo.append(userFollowers);
+  cardInfo.append(userFollowing);
+  cardInfo.append(userBio);
+  userProfile.append(profileAddress);
+
+  // add classes to elements
+  cardDiv.classList.add('card');
+  cardInfo.classList.add('card-info');
+  cardName.classList.add('name');
+  userName.classList.add('username');
+
+  // set text content
+  cardImg.src = object.data.avatar_url;
+  cardName.textContent = object.data.name;
+  userName.textContent = object.data.login;
+  userLocation.textContent = object.data.location;
+  userProfile.textContent = object.data.url;
+  profileAddress.href = object.data.html_url;
+  profileAddress.textContent = object.data.html_url;
+  userFollowers.textContent = `Followers: ${object.data.followers_url}`;
+  userFollowing.textContent = `Following: ${object.data.following_url}`;
+  userBio.textContent = object.data.bio;
+
+  return cardDiv;
+}
